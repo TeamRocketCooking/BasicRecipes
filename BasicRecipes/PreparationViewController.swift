cf5754531e7a893b1265c8a3c5a8cef410e9d43d
Yanni Speron <yannisperon@gmail.com>
2020-12-05 19:38:29 -0600

    Finished the code for the preparation page.

--- BasicRecipes/PreparationViewController.swift
+++ BasicRecipes/PreparationViewController.swift
@@ -8,20 +8,56 @@
 import UIKit
 import Parse
 
-class PreparationViewController: UIViewController {
+class PreparationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var preparationImage: UIImageView!
+    @IBOutlet weak var stepsTable: UITableView!
     
     var preparation: PFObject!
+    
+    var steps = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        stepsTable.delegate = self
+        stepsTable.dataSource = self
+        
         nameLabel.text = preparation["name"] as? String
         // Do any additional setup after loading the view.
+        
+        let imageFile = preparation["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        preparationImage.af.setImage(withURL: url)
+        
+        let preparationSteps = preparation["steps"] as? [String]
+        
+        if let unwrappedPreparationSteps = preparationSteps {
+            for step in unwrappedPreparationSteps {
+                steps.append(step)
+            }
+            stepsTable.reloadData()
+        } else {
+            print("Preparation steps not found!")
+        }
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return steps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PreparationStepCell") as! PreparationStepCell
+        
+        let step = steps[indexPath.row]
+        cell.stepLabel.text = "\(indexPath.row + 1). " + step
+        
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -30,7 +66,6 @@ class PreparationViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         
-    }
-    */
+    }*/
 
 }
